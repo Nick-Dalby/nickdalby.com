3bf76bec6ddb6bd2d6c5c70cf089f07a457a5dab
Nick Dalby <nckdlby@gmail.com>
2022-06-16 11:54:14 +0200

    smooth scrolling via css

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-const anchors = document.querySelectorAll('a');
-anchors.forEach(anchor => {
-  anchor.addEventListener('click', event => {
-    const href = anchor.getAttribute('href');
-    if (href.charAt(0) === '#') {
-      event.preventDefault();
-      document.querySelector(href).scrollIntoView({
-        behavior: 'smooth'
-      });
-    }
-  });
-});
 
 const sections = document.querySelectorAll('section')
 
